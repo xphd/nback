c2d5173fae039f50f1c447505cb35cb7adcf01fe
alex <you@example.com>
2020-03-30 23:51:32 -0400

    first kill

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <NBack/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import NBack from "./components/NBack.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    NBack
   }
 }
 </script>
---
+++ frontend/src/components/NBack.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>This is NBack</h1>
+    <!-- <button @click="getRandom">getRandom</button> -->
+    <p>{{ random }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NBack",
+  // data: local variables
+  data() {
+    return {
+      random: null,
+      interval: 1 // time interval in second
+    };
+  },
+  // mounted, what will do when mounted the page
+  mounted() {
+    setInterval(this.getRandom, 500)
+  },
+  // methods: useful functions used here
+  methods: {
+    getRandom() {
+      let number = Math.floor(Math.random() * 10);
+      this.random = number;
+    }
+  },
+
+  // sockets, for talk with backedn
+  sockets: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
