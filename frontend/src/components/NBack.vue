ac8b53866d40dbbb381b62657ab5d2bc49a2259c
alex <cong.cs.tufts@gmail.com>
2020-03-31 22:52:47 -0400

    update

--- frontend/src/components/NBack.vue
+++ frontend/src/components/NBack.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>This is NBack</h1>
+    <p>{{ randomSequence }}</p>
+    <!-- <p>{{ keydownSpaceSequence }}</p> -->
     <!-- <button @click="getRandom">getRandom</button> -->
+    <button @click="commence" :disabled="!terminated">Commence</button>
+    <button @click="terminate" :disabled="terminated">Terminate</button>
     <p>{{ random }}</p>
   </div>
 </template>
@@ -12,19 +16,76 @@ export default {
   // data: local variables
   data() {
     return {
+      terminated: true,
+      randomSequence: [1, 2, 3, 3, 4, 5],
+      currentIndex: 0,
       random: null,
-      interval: 1 // time interval in second
+      // interval: null, // setInterval function
+      keydownSpace: false,
+      keydownSpaceSequence: [false, false, false, false, false, false]
     };
   },
   // mounted, what will do when mounted the page
   mounted() {
-    setInterval(this.getRandom, 500)
+    // this.initializeRandomSequence();
+    let vm = this;
+    window.addEventListener("keydown", function(e) {
+      // 32 is the code for space
+      if (e.keyCode == 32 && !this.keydownSpace) {
+        // console.log(e.keyCode);
+        // this.keydownSpace = true;
+
+        let keydownSpaceSequence = vm.keydownSpaceSequence;
+        let currentIndex = vm.currentIndex;
+        keydownSpaceSequence[currentIndex] = true;
+        console.log(vm.keydownSpaceSequence);
+        // console.log(currentIndex);
+      }
+    });
+    // setInterval(()=>{
+    //   // if () {return}  // terminating condition
+    //     this.getRandom()
+    // }, 1500),
   },
   // methods: useful functions used here
   methods: {
+    initializeRandomSequence() {
+      console.log("initializeRandomSequence");
+      let numberOfElement = 11;
+      for (let i = 0; i < numberOfElement; i++) {
+        let randomElement = Math.floor(Math.random() * 10);
+        this.randomSequence.push(randomElement);
+      }
+    },
     getRandom() {
-      let number = Math.floor(Math.random() * 10);
-      this.random = number;
+      // console.log("getRandom");
+      let currentIndex = this.currentIndex;
+      let randomSequence = this.randomSequence;
+      let len = randomSequence.length;
+      if (currentIndex >= len - 1) {
+        this.terminate();
+      }
+      this.random = this.randomSequence[currentIndex];
+      this.currentIndex = currentIndex + 1;
+      setTimeout(() => {
+        this.random = null;
+      }, 1000);
+    },
+    // keydownSpace(){
+    //   console.log("keydown space")
+    // },
+    commence() {
+      this.terminated = false;
+      this.currentIndex = 0;
+      this.interval = setInterval(() => {
+        // if () {return}  // terminating condition
+        // this.keydownSpace = false;
+        this.getRandom();
+      }, 1500);
+    },
+    terminate() {
+      this.terminated = true;
+      clearInterval(this.interval);
     }
   },
 
